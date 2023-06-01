9b20b49ecd06ae8b108545a679466702e2b49d2c
s-batish <>
2023-06-01 11:06:23 +0000

    Edit settings.py for deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse('postgres://DATABASE_URL')
 }
 
 
