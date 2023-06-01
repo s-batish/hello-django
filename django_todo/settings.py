14288ad2957df67d5d2dda2c8fff723155496489
s-batish <>
2023-06-01 12:15:21 +0000

    Edit secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import env
 
 development = os.environ.get('DEVELOPMENT', False)
 
@@ -26,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 if os.path.isfile('env.py'):
     import env
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
