16460fbc1b3e41c4409e72ba449187cb40eca699
s-batish <>
2023-06-01 10:56:15 +0000

    Fix allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-s-batish-hello-django-q704da4bfh.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['django-todo-app-0.herokuapp.com']
 
 
 # Application definition
